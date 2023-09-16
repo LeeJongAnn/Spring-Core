e5db29f1ecdf9dde6a706e225add9e702a05626b
LeeJongAnn <dlwhddksiq@naver.com>
2023-09-16 20:42:43 +0900

    관심사의 분리 , DIP 위반한 내용 수정 및 추상화에 의존하도록 변경  + 테스트 코드 작성

--- src/test/java/hello/core/order/OrderServiceTests.java
+++ src/test/java/hello/core/order/OrderServiceTests.java
@@ -1,20 +1,31 @@
 package hello.core.order;
 
+import hello.core.AppConfig;
 import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class OrderServiceTests {
 
-    MemberService memberService = new MemberServiceImpl();
-    OrderService orderService = new OrderServiceImpl();
+
+    MemberService memberService;
+    OrderService orderService;
+
+    @BeforeEach
+    public void beforeEach() {
+
+        AppConfig appConfig = new AppConfig();
+        memberService = appConfig.memeberService();
+        orderService = appConfig.orderService();
+    }
 
     @Test
-    void createOrder(){
+    void createOrder() {
         Long memberId = 1L;
         Member jong = new Member(memberId, "Jong", Grade.VIP);
         memberService.join(jong);
